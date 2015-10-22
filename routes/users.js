c45feb330d99bbc76446537cb781fd41bbf64c8f
= <=>
2015-10-22 22:52:14 +0100

    Functionality to get user's classes

--- routes/users.js
+++ routes/users.js
@@ -22,21 +22,29 @@ exports.get_info = function(req, res) {
 
 	// Getting the Username from the request
 	var username = req.params.username;
+	var classes = {};
+	var task_name = {};
+	var task_desc = {};
+
 
 	// Getting info about the user
-	db.query('SELECT `stu_id`, `email`, `full_name` FROM users WHERE username = ? LIMIT 1', username, function(err, result) {
-		// Checking response
-		if (result.length < 1) {
+	db.query('select tasks.id "task_id", tasks.task_name "task_name", tasks.task_desc "task_desc", classes.class_name "class_name", classes.id "class_id" from tasks, classes where tasks.class_id = classes.id and tasks.class_id in (select classes.id from classes where classes.id in (select relations.class_id from relations where relations.student_id in (select users.id from users where users.username like ?)))', username, function(err, result) {
+
+		if (result.length > 0) {
+
+			for (i = 0; i < result.length; i++) {
+				classes[result[i].class_id] = result[i].class_name;
+			}
+
 			res.json({
-				status: 'ok',
-				warning: 'Failed to load user details...'
+				data: {
+					classes: classes
+				}
 			});
-		} else {
-			res.json({
-				status: 'ok',
-				data: result
-			});
+
 		}
+
 	});
 
 }
+
